5e4c949df8cb843d7bff9988ef22df5f7e1646a4
LowUp <60651716+LowUp@users.noreply.github.com>
2020-03-17 21:53:00 +0000

    Final Flash

--- CompiledTable.sql
+++ CompiledTable.sql
@@ -67,7 +67,7 @@ CYCLE
 NOCACHE;
 
 
---Bokoya
+
 CREATE SEQUENCE seq_VideoID 
 START WITH 1
 INCREMENT BY 1 
@@ -112,8 +112,8 @@ CREATE TABLE CHANNEL (
   
 CREATE TABLE VIDEO (
  vidArtificial_PK 	NUMBER NOT NULL,
- identifier			varchar(4) NOT NULL,
- title				varchar(14) NOT NULL,
+ identifier			varchar2(4) NOT NULL,
+ title				varchar2(14) NOT NULL,
  postDate		DATE NOT NULL,
  tags				varchar(10) NOT NULL,
  numberOfViews	NUMBER NOT NULL,
@@ -124,8 +124,8 @@ CONSTRAINT VIDEO_PRIMARY_KEY PRIMARY KEY (vidArtificial_PK));
 
 
 CREATE TABLE subscription(
-subArtificial_PK NUMBER NOT NULL,
-subCode VARCHAR2(6), 
+subArtificial_PK NUMBER NOT NULL UNIQUE,
+subCode VARCHAR2(6) UNIQUE, 
 subStart_Date DATE NOT NULL,
 subEnd_Date DATE,
 userID NUMBER NOT NULL,
@@ -224,12 +224,12 @@ VALUES ('clanzER', 'Legend45', 'A4');
 
 INSERT INTO USER_TABLE VALUES (seq_UserPrimaryID.NEXTVAL, 'user1', 'rooM', 'aRi', 'asimraii@yahoo.com', '23-AUG-1996');
 INSERT INTO USER_TABLE VALUES (seq_UserPrimaryID.NEXTVAL, 'user2', 'Misa', 'Iar', 'fr76@room.fr', '24-AUG-1997');
-INSERT INTO USER_TABLE VALUES (seq_UserPrimaryID.NEXTVAL, 'user3', 'Juan', 'Mendoza', 'juan@canterbury.ac.uk', '24-JAN-2007');
+INSERT INTO USER_TABLE VALUES (seq_UserPrimaryID.NEXTVAL, 'user3', 'Juan', 'Mendoza', 'juan@canterbury.ac.uk', '21-AUG-1999');
 
 INSERT INTO USER_TABLE (userName, firstName, lastName, emailAddress, dateOfBirth) 
-VALUES ('user67', 'roM', 'Ri', 'asdef@krukru.com', '27-AUG-1996');
+VALUES ('user67', 'roM', 'Ri', 'asdef@krukru.com', '12-AUG-1999');
 
-INSERT INTO subscription VAlUES (seq_SubscriptionPrimaryID.NEXTVAL, CONCAT('SUB', seq_SubNumber.NEXTVAL), TO_CHAR(SYSDATE, 'DD-MON-RR'),'24-JAN-2020', 1,1);
+INSERT INTO subscription VAlUES (seq_SubscriptionPrimaryID.NEXTVAL, CONCAT('SUB', seq_SubNumber.NEXTVAL), TO_CHAR(SYSDATE, 'DD-MON-RR'),'24-JAN-2020', 2,1);
 INSERT INTO subscription VAlUES (seq_SubscriptionPrimaryID.NEXTVAL, CONCAT('SUB', seq_SubNumber.NEXTVAL), TO_CHAR(SYSDATE, 'DD-MON-RR'),'25-JAN-2020', 1,1); 
 INSERT INTO subscription VAlUES (seq_SubscriptionPrimaryID.NEXTVAL, CONCAT('SUB', seq_SubNumber.NEXTVAL), TO_CHAR(SYSDATE, 'DD-MON-RR'),NULL, 2,1);
 INSERT INTO subscription VAlUES (seq_SubscriptionPrimaryID.NEXTVAL, CONCAT('SUB', seq_SubNumber.NEXTVAL), TO_CHAR(SYSDATE, 'DD-MON-RR'),NULL, 3,1);
@@ -242,8 +242,9 @@ INSERT INTO subscription (subEnd_Date, userID, channelID)
 VALUES ('14-MAR-2020',1,2);
 
 
-INSERT INTO VIDEO VALUES (seq_VideoID.NEXTVAL, CONCAT(dbms_random.string('U', 2), seq_videoIDNumber.NEXTVAL) , CONCAT('Video No', seq_ArtificialID.NEXTVAL), TO_CHAR(SYSDATE, 'DD-MON-RR'), 'Nature', 0, 1);
+INSERT INTO VIDEO VALUES (seq_VideoID.NEXTVAL, CONCAT(dbms_random.string('U', 2), seq_videoIDNumber.NEXTVAL), CONCAT('Video No', seq_ArtificialID.NEXTVAL), TO_CHAR(SYSDATE, 'DD-MON-RR'), 'Nature', 0, 1);
 INSERT INTO VIDEO VALUES (seq_VideoID.NEXTVAL, CONCAT(dbms_random.string('U', 2), seq_videoIDNumber.NEXTVAL), CONCAT('Video No', seq_ArtificialID.NEXTVAL), TO_CHAR(SYSDATE, 'DD-MON-RR'), 'Nature', 0, 1);  
+
 
 INSERT INTO VIDEO (identifier, title, postDate, tags, numberOfViews, channelID) 
 VALUES (CONCAT(dbms_random.string('U', 2), seq_videoIDNumber.NEXTVAL) , CONCAT('Video No', seq_ArtificialID.NEXTVAL), TO_CHAR(SYSDATE, 'DD-MON-RR'), 'Nature', 0, 1);
